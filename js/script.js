eb935463232cea389777f3dcc30f2668cec3ea15
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-10-06 15:14:52 +0200

    [add]: create newArray

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      newArray: [],
+      numberMail: 10,
+      
+    }
+  },
+
+  methods: {
+    getApi(numberMail){
+      for (let index = 0; index < numberMail; index++) {
+
+        axios.get(this.apiUrl)
+          .then((risposta) => {
+            this.newArray.push(risposta.data.response)
+    
+          })
+          
+          .catch((error) => {
+            console.log(error)
+        })
+      };
+
+    }
+    
+  },
+
+  mounted() {
+    this.getApi(this.numberMail)
+    console.log(this.newArray)
+  },
+
+
+}).mount('#app')
